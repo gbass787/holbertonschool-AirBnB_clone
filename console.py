8ef94a417b57ff9728ea7d60ce77452adb2384b0
Raulj06 <5015@holbertonschool.com>
2022-10-13 20:54:57 +0000

    console

--- console.py
+++ console.py
@@ -1,114 +1,72 @@
 #!/usr/bin/python3
-"""
-contains the entry point of the command interpreter
-
-"""
-
+"""Contains the entry potin of the command interpreter"""
 import cmd
 from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models.__init__ import storage
 from models import storage
 import json
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-
-    """
-    HBNB console
-    """
-
-	_classes = {
-	"BaseModel": BaseModel,
-	"User": User,
-	"Place": Place,
-	"State": State,
-	"City": City,
-	"Amenity": Amenity,
-	"Review": Review
-}
+    """HBNB console"""
 
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel": BaseModel(),
+        "User": User(),
+        "State": State(),
+        "City": City(),
+        "Place": Place(),
+        "Amenity": Amenity(),
+        "Review": Review()
+    }
 
-    def do_quit(self, arg):
-	"""
-	Quit command to exit the program:
-	- quit
+    def do_quit(self, args):
+        """Quit command to exit the program\n"""
+        exit()
 
-	"""
-	return True
-    
-    def do_EOF(self, arg):
-	"""
-	End Of File: exit the program
-	-EOF
+    def do_EOF(self, args):
+        """EOF command to exit the program\n"""
+        exit()
 
-	"""
-	return True
-
-    def help_quit(self):
-	"""This action is provided by default by cmd"""
-
-    print("Quit command to exit the program\n")
-	return True
-    def help_EOF(self):
-	"""EOF help"""
- 
-    print("End Of File: exit the program\n")
-	return True
-    
-    def emptyline(self):
-	"""
-	empty line + Enter should not execute anything
-	"""
-	return False
-
-    def do_create(self, arg):
-	"""
-	creates instance of Basemodel
-	save it to json and prints the id
-	"""
-	if not arg:
-	
-	print("** class name missing **")
-
-	elif args not in self.classes:
-	print("** class doesn't exist **")
-
-	else:
-	new = self.classes[args]
-	new.save()
-	print(new.id)
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it (to the JSON file)\n"""
+        if not args:
+            print("** class name missing **")
+        elif args not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            new = self.__classes[args]
+            new.save()
+            print(new.id)
 
     def do_show(self, args):
-	""" Prints the string representation of an instance based on class and id\n"""
-	rep = storage.all()
-	arg = args.split()
-	if not arg :
-	    print("** class name missing **")
-	elif arg[0] not in self.classes:
-	    print("** class doesn't exist **")
-	elif len(arg) == 1:
-	    print("** instance id missing **")
-
-	else:
-	    match = False
-	    for obj_id in rep:
-	    if arg[1] == rep[obj_id].id:
-	    print(rep[obj_id])
-	    match = True
-	    if match is not True:
-	    print("** no instance found **")
+        """Prints string representation for instance based on class and id\n"""
+        rep = storage.all()
+        arg = args.split()
+        if not arg:
+            print("** class name missing **")
+        elif arg[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            match = False
+            for obj_id in rep:
+                if arg[1] == rep[obj_id].id:
+                    print(rep[obj_id])
+                    match = True
+            if match is not True:
+                print("** no instance found **")
 
     def do_destroy(self, args):
-	""" Delete an instance based on the class name and id\n """
-
-	rep = storage.all()
+        """Deletes an instance based on the class name and id\n"""
+        rep = storage.all()
         arg = args.split()
         if not arg:
             print("** class name missing **")
@@ -136,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
 
     def do_all(self, args):
-        'Prints string representations of all instances\n'
+        """Prints string representations of all instances\n"""
         rep = storage.all()
         arg = args.split()
         if len(args) == 0:
@@ -154,7 +112,7 @@ class HBNBCommand(cmd.Cmd):
             print("{}".format(obj_list))
 
     def do_update(self, args):
-        'Updates an instance\n'
+        """Updates an instance\n"""
         arg_list = args.split()
         all_objs = storage.all()
         if len(arg_list) == 0:
@@ -177,6 +135,11 @@ class HBNBCommand(cmd.Cmd):
                     print("** value missing **")
                 else:
                     setattr(match, arg_list[2], arg_list[3])
+
+    def postcmd(self, stop, line):
+        if cmd.Cmd.do_help:
+            HBNBCommand().cmdloop()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
