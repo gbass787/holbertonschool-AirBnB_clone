0fc6c028032d3b983474b875b4672abc54c3a306
Raulj06 <5015@holbertonschool.com>
2022-10-13 21:49:12 +0000

    task 9

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" define a State class """
+ """ define a State class """
 from models.basemodel import BaseModel
 
 
 class State(BaseModel):
-""" Represent a State inherits from BaseModel """
+ """ Represent a State inherits from BaseModel """
 
 name = ""
