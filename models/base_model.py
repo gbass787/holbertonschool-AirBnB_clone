c81e52e148a05d834ee3b6c4cabdcd0f48700745
Raulj06 <5015@holbertonschool.com>
2022-10-14 22:38:24 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         '''Returns str representation of the instance'''
@@ -40,4 +39,5 @@ class BaseModel:
     def save(self):
         '''Saves object to JSON file'''
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
