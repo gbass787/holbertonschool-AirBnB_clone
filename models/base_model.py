15acc4a43b10660fbab8971e55aae66050baeaa6
gbass787 <4551@holbertonschool.com>
2022-10-15 01:36:01 +0000

    Still trying to fix base_model.py error

--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,49 @@
 #!/usr/bin/python3
-'class BaseModel that defines all common attributes/methods for other classes'
+'''class BaseModel that defines all common attributes/methods for other classes'''
 
-import uuid
 from datetime import datetime
 import models
+import uuid
+
+DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-class BaseModel:
-    '''BaseModel Class'''
+class BaseModel():
+    '''BaseModel class'''
     def __init__(self, *args, **kwargs):
         '''Initializes BaseModel attributes'''
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    dt = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, dt)
-                else:
-                    if key != "__class__":
-                        setattr(self, key, value)
-        else:
+        fmt = DATE_FORMAT
+
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
+        else:
+            del kwargs['__class__']
+            kwargs["created_at"] = datetime.strptime(kwargs['created_at'], fmt)
+            kwargs["updated_at"] = datetime.strptime(kwargs['updated_at'], fmt)
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         '''Returns str representation of the instance'''
-        c_n = self.__class__.__name__
-        return "[{}] ({}) {}".format(c_n, self.id, self.__dict__)
+        class_name = self.__class__.__name__
 
-    def to_dict(self):
-        '''Returns dictionary of all attributes of the BaseModel instance'''
-        new_dict_copy = self.__dict__.copy()
-        new_dict_copy['__class__'] = self.__class__.__name__
-        new_dict_copy['created_at'] = self.created_at.isoformat()
-        new_dict_copy['updated_at'] = self.updated_at.isoformat()
-        return new_dict_copy
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
         '''Saves object to JSON file'''
+
         self.updated_at = datetime.now()
         models.storage.save()
+
+    def to_dict(self):
+        '''Returns dictionary of all attributes of the BaseModel instance'''
+
+        todic = self.__dict__.copy()
+        todic["__class__"] = self.__class__.__name__
+        todic["created_at"] = self.created_at.isoformat()
+        todic["updated_at"] = self.updated_at.isoformat()
+
+        return todic
