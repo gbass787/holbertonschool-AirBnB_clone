802935b6e7607c99ac4d7ba943000d1d7e74cd87
gbass787 <4551@holbertonschool.com>
2022-10-15 02:40:39 +0000

    Fixing basemodel test file, deleted basemodel backup file, reverted to basemodel backup

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,27 +9,26 @@ import datetime
 
 
 class BaseModelTests(unittest.TestCase):
-    """ Suite of Console Tests """
+    '''unittests for our BaseModel class'''
 
     my_model = BaseModel()
 
     def testBaseModel1(self):
-        """ Test attributes value of a BaseModel instance """
+        '''testing values of attributes'''
 
-        self.my_model.name = "Holberton"
-        self.my_model.my_number = 89
+        self.my_model.name = "Holby"
+        self.my_model.my_number = 42
         self.my_model.save()
         my_model_json = self.my_model.to_dict()
 
-        self.assertEqual(self.my_model.name, "Holberton")
-        self.assertEqual(self.my_model.my_number, 89)
+        self.assertEqual(self.my_model.name, "Holby")
+        self.assertEqual(self.my_model.my_number, 42)
         self.assertEqual('BaseModel', my_model_json['__class__'])
         self.assertEqual(self.my_model.id, my_model_json['id'])
 
     def testSave(self):
-        """ Checks if save method updates the public instance instance
-        attribute updated_at """
-        self.my_model.first_name = "First"
+        '''Checks to see if save method updates updated_at'''
+        self.my_model.first_name = "Alpha"
         self.my_model.save()
 
         self.assertIsInstance(self.my_model.id, str)
@@ -38,7 +37,7 @@ class BaseModelTests(unittest.TestCase):
 
         first_dict = self.my_model.to_dict()
 
-        self.my_model.first_name = "Second"
+        self.my_model.first_name = "Omega"
         self.my_model.save()
         sec_dict = self.my_model.to_dict()
 
