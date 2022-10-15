84a84979b0c1bb37878f1b12d3fb742a72a51f67
gbass787 <4551@holbertonschool.com>
2022-10-15 01:05:06 +0000

    Fixed def save(self) in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         '''Returns str representation of the instance'''
